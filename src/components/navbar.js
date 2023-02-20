d2d45736eab11becba00b62d273d746c60ed9ee0
Abdelhak-Achour <achourabdelhak2001@gmail.com>
2023-02-20 22:34:12 +0100

    ver 1 navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Logo from "../images/logo_tucnospro.png";
 import "bulma/css/bulma.min.css";
 import {Link, NavLink} from "react-router-dom";
@@ -7,21 +7,46 @@ import emailIcon from '../images/email-161.svg';
 
 export function Navbar ()
 {
+
+    const [active, setActive] = useState(false);
+
+    function activateLinks()
+    {
+        setActive( (prev) => {
+            return !prev
+        })
+    }
+
     return(
         <>
             <nav className = "navbar has-shadow is-transparent">
                 <div className = "navbar-brand">
                     <Link className = "navbar-item py-4 px-6" to = "/"> <img className = "logo" src = {Logo} alt = "logo img" /></Link>
-                    <ul>
-                        <li className="navbar-item">
-                            <p><img src={phoneIcon} /> 92 999 737 | 27 643 085</p>
-                        </li>
-                        <li className="navbar-item">
-                            
-                        </li>
-                    </ul>
+                    <table className="info">
+                        <tr>
+                            <td>
+                                <img className="icon-custom" src={phoneIcon} alt = "phone-icon" />
+                            </td>
+                            <td>
+                                92 999 737 | 27 643 085
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <img className="icon-custom" src={emailIcon} alt = "email-icon" />
+                            </td>
+                            <td>
+                                tucnospro@gmail.com
+                            </td>
+                        </tr>
+                    </table>
+                    <a className="navbar-burger" onClick={activateLinks}>
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </a>
                 </div>
-                <div className = "navbar-menu" id = "nav-links">
+                <div className = {active ? "navbar-menu is-active" : "navbar-menu"} id = "nav-links">
                     <div className = "navbar-end">
                         <NavLink className = "navbar-item" to = "/">Accueil</NavLink>
                         <NavLink className = "navbar-item" to = "/a-propos">A propos</NavLink>
