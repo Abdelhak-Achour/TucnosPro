df533416718d0746ea19cbbf64b6f3849f6c994e
Abdelhak-Achour <achourabdelhak2001@gmail.com>
2023-02-22 22:56:12 +0100

    changed mobile navbar menu color and a few stuff

--- src/pages/home.js
+++ src/pages/home.js
@@ -4,8 +4,8 @@ export function Home()
     return (
         <>
             <div className = 'home-image'>
-                <h1>TucnosPro,</h1>
-                <h1>Se former, c'est Avancer</h1>
+                <p className='title is-1 has-text-centered'>TucnosPro,</p>
+                <p className='subtitle is-1 is-spaced has-text-centered'>Se former, c'est Avancer</p>
             </div>
             
             <h1>it works</h1>
