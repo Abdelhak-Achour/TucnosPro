d4ab1c60859002c617d2538f5dec76d2661d0db2
Abdelhak-Achour <achourabdelhak2001@gmail.com>
2023-03-17 19:44:33 +0100

    working on sub page

---
+++ src/pages/inscrire.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Navbar2 } from "../components/navbar2";
+
+export function Inscrire()
+{
+    return(
+        <>
+            <Navbar2 />
+            <p className="title has-text-dark m-6">sub page</p>
+        </>
+    )
+}
