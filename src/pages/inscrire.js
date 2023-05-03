47aac5e5d6f6f94eb4578fcc47d588e93927debe
Abdelhak-Achour <achourabdelhak2001@gmail.com>
2023-05-03 20:47:19 +0100

    sub form for a formation now sends data to backend and works for subbing

--- src/pages/inscrire.js
+++ src/pages/inscrire.js
@@ -1,7 +1,8 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { Navbar2 } from "../components/navbar2";
 import data from '../mock_data.json';
+import axios from "axios";
 
 export function Inscrire()
 {
@@ -13,18 +14,22 @@ export function Inscrire()
         if(snb === 0)
         {
             setSituations([true, false, false, false]);
+            setSituation("étudiant");
         }
         else if(snb === 1)
         {
             setSituations([false, true, false, false]);
+            setSituation("à la recherche d'un emploi");
         }
         else if(snb === 2)
         {
             setSituations([false, false, true, false]);
+            setSituation("en poste");
         }
         else
         {
             setSituations([false, false, false, true]);
+            setSituation("stagiare");
         }
     };
 
@@ -34,30 +39,37 @@ export function Inscrire()
         if(lnb === 0)
         {
             setLevels([true, false, false, false, false, false, false]);
+            setStudieslvl("lycée");
         }
         else if(lnb === 1)
         {
             setLevels([false, true, false, false, false, false, false]);
+            setStudieslvl("licence");
         }
         else if(lnb === 2)
         {
             setLevels([false, false, true, false, false, false, false]);
+            setStudieslvl("maitrise");
         }
         else if(lnb === 3)
         {
             setLevels([false, false, false, true, false, false, false]);
+            setStudieslvl("master");
         }
         else if(lnb === 4)
         {
             setLevels([false, false, false, false, true, false, false]);
+            setStudieslvl("ingénieur");
         }
         else if(lnb === 5)
         {
             setLevels([false, false, false, false, false, true, false]);
+            setStudieslvl("doctorat");
         }
         else
         {
             setLevels([false, false, false, false, false, false, true]);
+            setStudieslvl("autre");
         }
     };
 
@@ -67,23 +79,98 @@ export function Inscrire()
         if(tnb === 0)
         {
             setTypes([true, false]);
+            setType("présentiel");
         }
         else
         {
             setTypes([false, true]);
+            setType("en ligne");
         }
     };
+
+    const [formationData, setFormationData] = useState({formation: {}});
+
+    async function getFormationData()
+    {
+        try
+        {
+            var response = await axios.get(`http://localhost:3001/formation/${id}`);
+            setFormationData(response.data);
+        }
+        catch (err)
+        {
+            console.log(err);
+        }
+    }
+
+    useEffect(() => {
+        getFormationData();
+    }, [id]);
+
+    const [lastname, setLastname] = useState("");
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [addess, setAddress] = useState("");
+    const [phonenumber, setPhonenumber] = useState("");
+    const [situation, setSituation] = useState("");
+    const [studieslvl, setStudieslvl] = useState("");
+    const [studiesspecific, setStudiesspecific] = useState("");
+    const [speciality, setSpeciality] = useState("");
+    const [type, setType] = useState("");
+    const [questions, setQuestions] = useState("");
+    const [mail, setMail] = useState(false);
+    const [phone, setPhone] = useState(false);
+    const [validate, setValidate] = useState(false);
+
+    async function submitHandle(event)
+    {
+        let contactmethod = "";
+        if(mail)
+        {
+            contactmethod = contactmethod + "mail-";
+        }
+        if(phone)
+        {
+            contactmethod = contactmethod + "phone-";
+        }
+        if(validate)
+        {
+            contactmethod = contactmethod + "validate";
+        }
+
+        try
+        {
+            const response = await axios.post("http://localhost:3001/formation/inscrire", {
+                id,
+                lastname,
+                name,
+                email,
+                addess,
+                phonenumber,
+                situation,
+                studieslvl,
+                studiesspecific,
+                speciality,
+                type,
+                contactmethod,
+                questions
+            });
+        }
+        catch (err)
+        {
+            console.error(err)
+        }
+    }
 
     return(
         <>
             <Navbar2 />
             <div className="temoin">
-                <p className="title is-1 has-text-dark is-family-primary">S'inscrire pour la formation {data.formations[id-1].name}</p>
+                <p className="title is-1 has-text-dark is-family-primary">S'inscrire pour la formation {formationData.formation.name ? formationData.formation.name : ""}</p>
                 <div className="box temoin-form-div">
-                    <form className="has-text-left-desktop">
+                    <form className="has-text-left-desktop" onSubmit={submitHandle}>
                         <div className="columns">
                             <div className="column">
-
                                 <div className="field">
                                     <div className="columns is-mobile">
                                         <div className="column pb-0 pr-0 is-narrow">
@@ -94,7 +181,7 @@ export function Inscrire()
                                         </div>
                                     </div>
                                     <div className="control">
-                                        <input className="input" type="lastname" placeholder="Votre nom" />
+                                        <input className="input" type="lastname" placeholder="Votre nom" onChange={(event) => setLastname(event.target.value)} required />
                                     </div>
                                 </div>
                             </div>
@@ -110,7 +197,7 @@ export function Inscrire()
                                         </div>
                                     </div>
                                     <div className="control">
-                                        <input className="input" type="name" placeholder="Votre prénom" />
+                                        <input className="input" type="name" placeholder="Votre prénom" onChange={(event) => setName(event.target.value)} required />
                                     </div>
                                 </div>
                             </div>
@@ -126,7 +213,7 @@ export function Inscrire()
                                 </div>
                             </div>
                             <div className="control">
-                                <input className="input" type="email" placeholder="Votre Email" />
+                                <input className="input" type="email" placeholder="Votre Email" onChange={(event) => setEmail(event.target.value)} required />
                             </div>
                         </div>
 
@@ -140,7 +227,7 @@ export function Inscrire()
                                 </div>
                             </div>
                             <div className="control">
-                                <input className="input" type="address" placeholder="Votre Addresse" />
+                                <input className="input" type="address" placeholder="Votre Addresse" onChange={(event) => setAddress(event.target.value)} required />
                             </div>
                         </div>
 
@@ -154,7 +241,7 @@ export function Inscrire()
                                 </div>
                             </div>
                             <div className="control">
-                                <input className="input" type="phonenb" placeholder="Votre Numéro de téléphone" />
+                                <input className="input" type="phonenb" placeholder="Votre Numéro de téléphone" onChange={(event) => setPhonenumber(event.target.value)} required />
                             </div>
                         </div>
 
@@ -238,7 +325,7 @@ export function Inscrire()
                                 </div>
                             </div>
                             <div className="control">
-                                <input className="input" type="phonenb" placeholder="Spécifiez votre niveau d'étude" />
+                                <input className="input" type="phonenb" placeholder="Spécifiez votre niveau d'étude" onChange={(event) => setStudiesspecific(event.target.value)} />
                             </div>
                         </div>
 
@@ -252,7 +339,7 @@ export function Inscrire()
                                 </div>
                             </div>
                             <div className="control">
-                                <input className="input" type="phonenb" placeholder="Votre spécialité" />
+                                <input className="input" type="phonenb" placeholder="Votre spécialité" onChange={(event) => setSpeciality(event.target.value)} required />
                             </div>
                         </div>
 
@@ -288,19 +375,19 @@ export function Inscrire()
                             </div>
                             <div className="control">
                                 <label className="checkbox mr-2">
-                                    <input type="checkbox" value="mail"/>
+                                    <input type="checkbox" value="mail" onClick={() => setMail(!mail)}/>
                                     Je veux être contacté par mail par un responsable
                                 </label>
                             </div>
                             <div className="control">
                                 <label className="checkbox mr-2">
-                                    <input type="checkbox" value="phone"/>
+                                    <input type="checkbox" value="phone" onClick={() => setPhone(!phone)}/>
                                     Je veux être contacté par téléphone par un responsable
                                 </label>
                             </div>
                             <div className="control">
                                 <label className="checkbox mr-2">
-                                    <input type="checkbox" value="validate"/>
+                                    <input type="checkbox" value="validate" onClick={() => setValidate(!validate)}/>
                                     Je veux valider mon inscription et payer les frais de la formation directement
                                 </label>
                             </div>
@@ -312,18 +399,17 @@ export function Inscrire()
                                     <label className="label">Si vous avez des questions</label>
                                 </div>
                                 <div className="column pb-0 pr-0 is-narrow">
-                                    <p className="orange-star">*</p>
+                                    <p className="orange-star"></p>
                                 </div>
                             </div>
                             <div className="control">
-                                <textarea class="textarea" placeholder="Vos questions" rows="7"></textarea>
+                                <textarea class="textarea" placeholder="Vos questions" rows="7" onChange={(event) => setQuestions(event.target.value)}></textarea>
                             </div>
                         </div>
 
                         <div className="control">
-                            <button className="button is-link">S'inscrire</button>
+                            <button className="button is-link" type="submit">S'inscrire</button>
                         </div>
-
                     </form>
                 </div>
             </div>
